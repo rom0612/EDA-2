23b3d824ce37f951f8ca50cdf5e81a288bbc9956
rom <roman.ramos.322@gmail.com>
2025-08-22 00:20:28 -0600

    bubbleSort

---
+++ Practica1_AlgOrd/e3_bubbleSort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void BubbleSort(int lista[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (lista[j] > lista[j + 1]) {
+                // Intercambiar elementos
+                int temp = lista[j];
+                lista[j] = lista[j + 1];
+                lista[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        
+        printf("Lista después de iteración %d:\n", i + 1);
+        for (int k = 0; k < n; k++) {
+            printf("%d ", lista[k]);
+        }
+        printf("\n");
+        
+        // Si no hubo intercambios, el array ya está ordenado
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int lista[] = {25, 10, 18, 40, 3, 35, 2, 8, 21, 23, 300, 150, 30, 14, 27, 29, 48, 23, 19, 2};
+    int n = 20;  
+
+    printf("LISTA ORIGINAL:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", lista[i]);
+    }
+    printf("\n\n");
+    
+    BubbleSort(lista, n);
+    
+    printf("\nLista ordenada:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", lista[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
