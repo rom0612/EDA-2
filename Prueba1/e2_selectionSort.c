125956109fe595e88914d83dcb7d15a1e50fcfc0
rom <roman.ramos.322@gmail.com>
2025-08-22 09:05:12 -0600

    p1hide

--- Prueba1/e2_selectionSort.c
+++ Prueba1/e2_selectionSort.c
@@ -22,8 +22,8 @@ void SelectionSort(int lista[], int n) {
 }
 
 int main() {
-    int lista[] = {25, 10, 18, 40, 3, 35, 2, 8, 21, 23, 300, 150, 30, 14, 27, 29, 48, 23, 19, 2};
-    int n = 20;  
+    int lista[] = {25, 10, 18, 40, 3, 35, 2, 8};
+    int n = 8;  
 
     printf("LISTA ORIGINAL:\n");
     for (int i = 0; i < n; i++) {
